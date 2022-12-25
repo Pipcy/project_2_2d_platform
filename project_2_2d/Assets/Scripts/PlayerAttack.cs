0407f51eaffc33a1529a6a51aafac361a8d239e2
piporhicyan <79942890+piporhicyan@users.noreply.github.com>
2022-12-25 03:43:07 -0500

    point and shoot

--- project_2_2d/Assets/Scripts/PlayerAttack.cs
+++ project_2_2d/Assets/Scripts/PlayerAttack.cs
@@ -12,6 +12,11 @@ public class PlayerAttack : MonoBehaviour
     private PlayerMovement playerMovement;
     private float cooldownTimer = Mathf.Infinity;
 
+    //mouse aiming
+    public Camera cam;
+    Vector2 mousePos;
+    //
+
     void Start()
     {
         anim = GetComponent<Animator>();
@@ -32,8 +37,23 @@ public class PlayerAttack : MonoBehaviour
         anim.SetTrigger("attack");
         cooldownTimer = 0;//
 
+        //mouse aiming code
+        // Vector2 lookDir = mousePos - body.position;
+        // float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 0f;
+
+        // if (angle > -90 && angle < 90) 
+        // {
+            
+        // }else{ 
+        //     bullets[FindBullet()].transform.rotation = firePoint.rotation - 180f;//*** added this line myself to allow
+        //                                                               // bullets to be shoot 360 degree // learned from point to shoot Í
+        // }
+        //
+
         //pool fireballs
         bullets[FindBullet()].transform.position = firePoint.position;//set the pos of the bullet from
+        bullets[FindBullet()].transform.rotation = firePoint.rotation;//*** added this line myself to allow
+                                                                      // bullets to be shoot 360 degree // learned from point to shoot Í
         bullets[FindBullet()].GetComponent<Projectile>().SetDirection(-Mathf.Sign(transform.localScale.x));//set the direction the bullets
 
 
--- project_2_2d/Assets/Scripts/PlayerMovement.cs
+++ project_2_2d/Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,10 @@ public class PlayerMovement : MonoBehaviour
     private Animator anim;//**
     private bool grounded;
 
+    //mouse aiming
+    public Camera cam;
+    Vector2 mousePos;
+    //
 
     void Start()
     {
@@ -23,12 +27,26 @@ public class PlayerMovement : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         body.velocity = new Vector2( horizontalInput* speed,body.velocity.y);
 
+        transform.localScale = new Vector3(-3,3,3);//stay facing right (****unless mouse point to the left of the player)
         //fliping the 2d left and right
-        if (horizontalInput < 0f) // input ranges from -1 to 1, positive being right side
-            transform.localScale = new Vector3(3,3,3);//picture default move left(do nothing)
-        else if (horizontalInput >= 0f) 
-            transform.localScale = new Vector3(-3,3,3); //flip image when move right hence change x to -
+        // if (horizontalInput < 0f) // input ranges from -1 to 1, positive being right side
+        //     transform.localScale = new Vector3(3,3,3);//picture default move left(do nothing)
+        // else if (horizontalInput >= 0f) 
+        //     transform.localScale = new Vector3(-3,3,3); //flip image when move right hence change x to -
 
+        Vector2 lookDir = mousePos - body.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 0f;
+        body.rotation = angle;
+
+        //*** -90<angle<90 means the mouse rest on the right of the player
+        if (angle > -90 && angle < 90) //shot front face front (normal)
+        {
+            Debug.Log("right");
+            body.rotation = angle;
+        }else{ // shoot back face back
+            Debug.Log("left");
+            transform.localScale = new Vector3(-3,-3,3);
+        }
 
         //jump
         if(Input.GetKey(KeyCode.Space) && grounded)
@@ -40,6 +58,11 @@ public class PlayerMovement : MonoBehaviour
         //"horizontalInput != 0" return 1 when input is not zero,
         //and 0 when input is 0, a clever way to shorten the code
         anim.SetBool("grounded", grounded);
+
+
+        //aiming at mouse position
+        //covert from screen point to world point
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
     }
 
     //jump method **
