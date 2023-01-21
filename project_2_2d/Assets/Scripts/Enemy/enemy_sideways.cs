5c963da5d57e25cea85f93b6001fc88cc8c26f67
piporhicyan <piporhicyan@gmail.com>
2023-01-21 09:17:13 -0500

    fixes

--- project_2_2d/Assets/Scripts/Enemy/enemy_sideways.cs
+++ project_2_2d/Assets/Scripts/Enemy/enemy_sideways.cs
@@ -8,11 +8,24 @@ public class enemy_sideways : MonoBehaviour
     [SerializeField] private float damage;
     //deleted a bunch, didnt use the tutorial's way, used pill game waypoint follower logic instead
 
+    private bool killed = false;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
         {
-            collision.GetComponent<Health>().TakeDamage(damage);
+            if (!killed)
+                collision.GetComponent<Health>().TakeDamage(damage);
+        }
+        else if (collision.tag == "Bullet")
+        {
+            if (!killed)//*
+            {
+                gameObject.GetComponent<SpriteRenderer>().enabled = false;//*
+                killed = true;//*
+                //*fixes 11.3
+                //gameObject.SetActive(false); // deactivate heart so it wont add multiple health
+            }
         }
     }
 }
